501a7762d869c19f8221bd8c0dad8f0d5db2b17b
Keith Douglas <keith.douglas@codeclan.com>
2020-09-30 09:56:29 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send("<html><p><b>Hello World</b></p></html>")
+})
+
+app.listen(port, () => {
+  console.log("Example app running on port 3000")
+})
+
+
+
